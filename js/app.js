4959109d2b516b39677c1c5af2a1aa39762c91ce
HasegawaTaizou <caiocoghi@gmail.com>
2023-05-04 00:05:20 -0300

    qalo

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@
 import { fillAlbumArtist } from "./fill-album-artist.js";
 import { fillMusicArtist } from "./fill-music-artist.js";
 import { fillSearchArtist } from "./fill-search-artist.js";
-// import { getArtist } from "./get-artist.js";
+import { getArtist } from "./get-artist.js";
 
 // route;
 fillAlbumArtist;
 fillMusicArtist;
 fillSearchArtist;
-// getArtist;
+getArtist;
